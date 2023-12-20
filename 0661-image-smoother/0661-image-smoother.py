6f9ce6e4885c49bf49c0ad7b5537b55b013b3c79
JimmyYang <105638230+komex8500@users.noreply.github.com>
2023-12-20 16:51:17 +0800

    Time: 389 ms (81.94%), Space: 18.1 MB (14.21%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.py
@@ -0,0 +1,20 @@
+from typing import *
+
+
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        l1 = len(img)
+        l2 = len(img[0])
+        result = [[0] * l2 for _ in range(l1)]
+        for j in range(l1):
+            for k in range(l2):
+                sum = 0
+                size = 0
+                for x in range(j-1, j+2):
+                    for y in range(k-1, k+2):
+                        if x < 0 or y < 0 or x > l1-1 or y > l2-1:
+                            continue
+                        sum += img[x][y]
+                        size += 1
+                result[j][k] = sum//size
+        return result
