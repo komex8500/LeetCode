f365cac4a706abb36c7111b9bb5f0cf2e8694b17
JimmyYang <105638230+komex8500@users.noreply.github.com>
2023-12-25 00:14:44 +0800

    Time: 49 ms (69.33%), Space: 17.5 MB (5.67%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -1,24 +1,9 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        if s[0] == '0':
-            f0 = 0
-            f1 = 1
-        else:
-            f0 = 1
-            f1 = 0
-        for i in range(1, len(s)):
-            if i % 2 == 1:
-                # '0' is first
-                if s[i] == '0':
-                    f0 += 1
-                # '1' is first
-                if s[i] == '1':
-                    f1 += 1
+        f0, f1 = 0, 0
+        for i, c in enumerate(s):
+            if (i % 2 == 1 and c == '0') or (i % 2 == 0 and c == '1'):
+                f0 += 1
             else:
-                # '0' is first
-                if s[i] == '1':
-                    f0 += 1
-                # '1' is first
-                if s[i] == '0':
-                    f1 += 1
+                f1 += 1
         return min(f0, f1)
