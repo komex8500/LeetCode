7d1dc83d9cabe57ef2c4a0c714f654cb60d34f5c
JimmyYang <105638230+komex8500@users.noreply.github.com>
2024-01-14 19:31:04 +0800

    Time: 229 ms (29.08%), Space: 18.3 MB (28.83%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -2,13 +2,13 @@ class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         if len(word1) != len(word2):
             return False
-        list1 = [0] * 26
-        list2 = [0] * 26
+        dict1 = dict()
+        dict2 = dict()
         for i in range(len(word1)):
-            if word1[i] not in word2:
-                return False
-            list1[ord(word1[i]) - ord('a')] += 1
-            list2[ord(word2[i]) - ord('a')] += 1
-        list1.sort()
-        list2.sort()
-        return list1 == list2
+            dict1[word1[i]] = dict1.get(word1[i], 0) + 1
+            dict2[word2[i]] = dict2.get(word2[i], 0) + 1
+        if dict1.keys() != dict2.keys():
+            return False
+        list1 = list(dict1.values())
+        list2 = list(dict2.values())
+        return sorted(list1) == sorted(list2)
