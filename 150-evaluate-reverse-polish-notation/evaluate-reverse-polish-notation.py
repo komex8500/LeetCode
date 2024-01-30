d3644b902da5405f3ce22f1abe203f3c7200c3ea
JimmyYang <105638230+komex8500@users.noreply.github.com>
2024-01-30 16:28:22 +0800

    Time: 58 ms (95.28%) | Memory: 17.2 MB (62.53%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,19 +1,19 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        s = []
-        for i in range(len(tokens)):
-            string = tokens[i]
-            if string.isdigit() or (string[0] == "-" and string[1:].isdigit()):
-                s.append(int(tokens[i]))
+        stack = []
+        for i in tokens:
+            if i == '+':
+                stack.append(stack.pop() + stack.pop())
+            elif i == '-':
+                n1 = stack.pop()
+                n2 = stack.pop()
+                stack.append(n2 - n1)
+            elif i == '*':
+                stack.append(stack.pop() * stack.pop())
+            elif i == '/':
+                n1 = stack.pop()
+                n2 = stack.pop()
+                stack.append(int(n2 / n1))
             else:
-                n2 = s.pop()
-                n1 = s.pop()
-                if tokens[i] == "+":
-                    s.append(n1+n2)
-                elif tokens[i] == "-":
-                    s.append(n1-n2)
-                elif tokens[i] == "*":
-                    s.append(n1*n2)
-                elif tokens[i] == "/":
-                    s.append(int(n1/n2))
-        return s.pop()
+                stack.append(int(i))
+        return stack[0]
